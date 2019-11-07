cb94353719a26e7e69b872dff65ff14303d88258
l13n01d19 <l13n01d19@gmail.com>
2019-11-07 19:06:27 +0800

    Revert "debug"
    This reverts commit ee868794cc03389072d80ba119e026ce49f85775.

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] agrs) {
-            Scanner scan = new Scanner(System.in);
-            Calculate cal = new Calculate(scan.nextLine());
-            System.out.println(cal.calculation());
+        Scanner scan = new Scanner(System.in);
+        Calculate cal = new Calculate(scan.nextLine());
+        System.out.print(cal.calculation());
     }
 }
 
